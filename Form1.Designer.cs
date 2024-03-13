63dc5ed77b2138b473444720335d8f6a51a94c27
y-at <wyattbristow@hotmail.com>
2024-03-12 19:33:11 -0600

    Initial commit, v0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace iRacing_Steering_Wheel_Swap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.addWheelButton = new System.Windows.Forms.Button();
+            this.newWheelName = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // addWheelButton
+            // 
+            this.addWheelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.addWheelButton.AutoSize = true;
+            this.addWheelButton.Location = new System.Drawing.Point(598, 415);
+            this.addWheelButton.Name = "addWheelButton";
+            this.addWheelButton.Size = new System.Drawing.Size(106, 23);
+            this.addWheelButton.TabIndex = 0;
+            this.addWheelButton.Text = "Add Wheel...";
+            this.addWheelButton.UseVisualStyleBackColor = true;
+            this.addWheelButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // newWheelName
+            // 
+            this.newWheelName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.newWheelName.Location = new System.Drawing.Point(408, 415);
+            this.newWheelName.MaxLength = 30;
+            this.newWheelName.Name = "newWheelName";
+            this.newWheelName.Size = new System.Drawing.Size(184, 20);
+            this.newWheelName.TabIndex = 1;
+            this.newWheelName.Text = "New wheel name...";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(727, 397);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(12, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Overwrite disabled";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 420);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(727, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.newWheelName);
+            this.Controls.Add(this.addWheelButton);
+            this.Name = "Form1";
+            this.Text = "iRacing Wheel Swapper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addWheelButton;
+        private System.Windows.Forms.TextBox newWheelName;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace iRacing_Steering_Wheel_Swap
+{
+    public partial class Form1 : Form
+    {   
+        string iRacingDirectoryPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\iRacing";
+        string iRacingControlsDirectoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "iRacing\\controls\\");
+        bool owstate = false;
+        List<string> controlList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            CreateDirectoryForActiveUser();
+            populateWheelMenu();
+        }
+
+        private void CreateDirectoryForActiveUser()
+        {
+            // Check if the directory exists
+            if (!Directory.Exists(iRacingControlsDirectoryPath))
+            {
+                // Create the directory if it doesn't exist
+                Directory.CreateDirectory(iRacingControlsDirectoryPath);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create wheel file name from textbox input
+            string wheelName = newWheelName.Text + ".cfg";
+            // Directories for orignial cfg and where custom cfg's are stored
+            string originalControlPath = iRacingDirectoryPath + "\\controls.cfg";
+            string newControlPath = iRacingControlsDirectoryPath + wheelName;
+            // Throw exception if name field is empty
+            if (newWheelName.Text == "")
+            {
+                throw new Exception("Error: Must name control file.");
+            }
+            // Copy currently in use cfg to control directory
+            File.Copy(originalControlPath, newControlPath, owstate);
+            // Populate menu with pre-existing + newly added wheel
+            populateWheelMenu();
+        }
+
+        private void populateWheelMenu()
+        {
+            // Get list of .cfg files in controls directory
+            DirectoryInfo di = new DirectoryInfo(iRacingControlsDirectoryPath);
+            FileInfo[] cfgFiles = di.GetFiles("*.cfg");
+            // Clear pre-existing panels to avoid duplicates
+            flowLayoutPanel1.Controls.Clear();
+            controlList.Clear();
+            // Give prompt in GUI if no cfg files exist
+            if (cfgFiles.Length == 0)
+            {   
+                label1.Text = "No cfg files in directory.";
+            }
+            else {
+                // Remove GUI no cfg prompt 
+                label1.Text = "";
+                // Iterate through total cfg files to draw panels
+                for (int i = 0; i < cfgFiles.Length; i++)
+                {
+                    // Get cfg files from directory
+                    controlList.Append(iRacingControlsDirectoryPath + cfgFiles[i]);
+                    string wheelName = cfgFiles[i].Name;
+                    // Panel characteristics
+                    Panel panel;
+                    panel = new Panel();
+                    panel.Name = wheelName;
+                    panel.BackColor = Color.DarkGray;
+                    panel.BorderStyle = BorderStyle.FixedSingle;
+                    panel.Size = new Size(120, 120);
+                    panel.Margin = new Padding(10);
+                    // Label of each panel
+                    Label label;
+                    label = new Label();
+                    label.Name = wheelName;
+                    label.Text = wheelName;
+                    label.Location = new Point(0, 0);
+                    label.ForeColor = Color.White;
+                    label.AutoSize = true;
+                    // Event handler used to load the double clicked config file
+                    panel.DoubleClick += new EventHandler(Edit_DoubleClick);
+                    foreach (Control c in panel.Controls)
+                    {
+                        c.DoubleClick += new EventHandler(Edit_DoubleClick);
+                        Debug.WriteLine("Double click");
+                    }
+                    // Append label to panel
+                    panel.Controls.Add(label);
+                    // Add panels to flow panel
+                    flowLayoutPanel1.Controls.Add(panel);
+                }
+                
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // Toggle overwrite state
+            if (owstate == true)
+            {
+                owstate = false;
+                button1.Text = "Overwrite disabled";
+            }
+            else
+            {
+                owstate = true;
+                button1.Text = "Overwrite enabled";
+            }
+        }
+
+        private void Edit_DoubleClick(object sender, EventArgs e)
+        {
+            Control c = (Control)sender;
+            string chosenControlFile = (string)c.Name;
+            Debug.WriteLine(chosenControlFile);
+            File.Copy(iRacingControlsDirectoryPath+chosenControlFile, iRacingDirectoryPath+"\\controls.cfg", true);
+            System.Windows.Forms.MessageBox.Show("Preset loaded.");
+            Debug.WriteLine(iRacingControlsDirectoryPath + chosenControlFile);
+            Debug.WriteLine(iRacingDirectoryPath + "\\" +chosenControlFile);
+        }
+    }
+}
